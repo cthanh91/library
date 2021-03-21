1c4b265ae061c585f77f755f545faeebd9f191e9
cthanh91 <thanh@codelink.io>
2021-03-21 17:44:28 +0700

    Rename method

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,26 +4,26 @@ const user = require("../controller/user");
 const authentication = require("../controller/authentication");
 const book = require("../controller/book");
 const borrowing = require("../controller/borrowing");
-const authorization = require("../middleware/authorization");
+const isLoggedIn = require("../middleware/authorization");
 
 router.post("/login", authentication.login);
-router.post("/logout", authorization, authentication.logout);
+router.post("/logout", isLoggedIn, authentication.logout);
 
-router.get("/book", authorization, book.getAll);
-router.post("/book", authorization, book.create);
-router.put("/book/:id", authorization, book.update);
-router.delete("/book/:id", authorization, book.destroy);
-router.get("/book/search", authorization, book.search);
-router.post("/book/:id/borrow", authorization, book.borrow);
+router.get("/book", isLoggedIn, book.getAll);
+router.post("/book", isLoggedIn, book.create);
+router.put("/book/:id", isLoggedIn, book.update);
+router.delete("/book/:id", isLoggedIn, book.destroy);
+router.get("/book/search", isLoggedIn, book.search);
+router.post("/book/:id/borrow", isLoggedIn, book.borrow);
 
-router.get("/borrowing", authorization, borrowing.getAll);
-router.delete("/borrowing/:id", authorization, borrowing.destroy);
+router.get("/borrowing", isLoggedIn, borrowing.getAll);
+router.delete("/borrowing/:id", isLoggedIn, borrowing.destroy);
 
-router.get("/user", authorization, user.getAll);
-router.post("/user", authorization, user.create);
-router.put("/user/:id", authorization, user.update);
-router.delete("/user/:id", authorization, user.destroy);
+router.get("/user", isLoggedIn, user.getAll);
+router.post("/user", isLoggedIn, user.create);
+router.put("/user/:id", isLoggedIn, user.update);
+router.delete("/user/:id", isLoggedIn, user.destroy);
 
-router.get("/identity", authorization, authentication.checkIdentity);
+router.get("/identity", isLoggedIn, authentication.checkIdentity);
 
 module.exports = router;
