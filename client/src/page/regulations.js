df48a63cabeecd0d3c50035d1f476d929a918807
cthanh91 <thanh@codelink.io>
2021-03-28 18:12:18 +0700

    Update regulations content

--- client/src/page/regulations.js
+++ client/src/page/regulations.js
@@ -18,7 +18,16 @@ const Regulations = () => {
     <Template>
       <Container maxWidth="md" className={classes.container}>
         <h1>Regulations</h1>
-        <p>Content</p>
+        <ul>
+          <li>
+            You need to return book to Library within 24 hous after clicking Return button on
+            website, if not your account will be blocked for 6 months.
+          </li>
+          <li>If you lost the book, you have to pay for it.</li>
+          <li>
+            If you lost the Library card, you have to pay 50.000 VND.
+          </li>
+        </ul>
       </Container>
     </Template>
   );
